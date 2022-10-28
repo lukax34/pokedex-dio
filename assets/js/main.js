b279acc04e798bb6fda045e6de2877e80d3f2f02
Lucas Feliphe Aleixo <lukax34@outlook.com>
2022-10-28 10:53:42 -0300

    Conversão de modelo da API para próprio e alteração de cores de acordo com tipo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,18 @@ const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 
+
 function convertPokemonToHtml(pokemon){
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span> 
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -23,14 +23,8 @@ function convertPokemonToHtml(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => { 
-    const listItems = []
-    
-
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToHtml(pokemon))
-    }  
+pokeApi.getPokemons().then((pokemons = []) => { 
+    const newHtml = pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+    pokemonList.innerHTML += newHtml   
 })
     
